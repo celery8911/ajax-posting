77ff7f29b852a19c4736de3cac703efa481abb62
Celery8911 <celery8911@gmail.com>
2017-10-12 07:31:46 +0800

    2-7 Ajax 新增

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,14 +9,14 @@ class PostsController < ApplicationController
        @post.user = current_user
        @post.save
 
-       redirect_to posts_path
+       
      end
 
      def destroy
        @post = current_user.posts.find(params[:id]) # 只能删除自己的贴文
        @post.destroy
 
-        
+
      end
 
      protected
