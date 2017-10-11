45e8c8410baff5c8379987b8416fd1c2f32d624e
Celery8911 <celery8911@gmail.com>
2017-10-12 07:52:52 +0800

    2-10 Ajax 按讚

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,7 +25,7 @@ class PostsController < ApplicationController
            Like.create( :user => current_user, :post => @post)
          end
 
-         redirect_to posts_path
+          
        end
 
        def unlike
@@ -33,9 +33,9 @@ class PostsController < ApplicationController
          like = @post.find_like(current_user)
          like.destroy
 
-         redirect_to posts_path
+         render "like"
        end
-     
+
 
      protected
 
